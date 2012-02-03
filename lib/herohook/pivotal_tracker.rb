96d18218833a91ff96b9a4b8850690824c2cfa55
Evan Whalen <evanwhalen@gmail.com>
2012-02-03 14:53:09 -0500

    do not send email if no stories exist, handle nil config options

--- lib/herohook/pivotal_tracker.rb
+++ lib/herohook/pivotal_tracker.rb
@@ -9,17 +9,20 @@ module Herohook
       ::PivotalTracker::Client.token = config["api_token"]
       ::PivotalTracker::Client.use_ssl = true
       config["emails"].each_value do |email_config|
-        mail_settings = email_config.merge(config["mail_settings"])
-        mail_settings[:body] = mail_settings[:body].gsub(/%{app}/, app) << double_break << stories_body(email_config["pivotal_tracker_options"])
-        ::Pony.mail(mail_settings)
+        stories = stories(email_config["pivotal_tracker_options"])
+        unless stories.empty?
+          mail_settings = email_config.merge(config["mail_settings"])
+          mail_settings[:body] = mail_settings[:body].gsub(/%{app}/, app) << double_break << stories_body(stories)
+          ::Pony.mail(mail_settings)
+        end
       end
     end
     
-    def stories_body(options)
-      stories(options).map{|story| [story.name, story.url, story.description].join(?\n) }.join(double_break)
+    def stories_body(stories)
+      stories.map{|story| [story.name, story.url, story.description].join(?\n) }.join(double_break)
     end
     
-    def stories(options)
+    def stories(options = {})
       default_options = {:search => ["id", story_ids.join(",")].join(":")}
       project.stories.all(default_options.merge(options))
     end
