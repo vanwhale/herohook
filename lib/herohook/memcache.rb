5713d314cf668c38aa07eaf998ff5b714f266eea
Evan Whalen <evanwhalen@gmail.com>
2012-12-27 16:03:15 -0500

    Use symbol not string key for memcache config.

--- lib/herohook/memcache.rb
+++ lib/herohook/memcache.rb
@@ -5,7 +5,7 @@ module Herohook
   class Memcache < Base
     
     def perform
-      heroku_client = Heroku::Client.new(config["heroku_user"], config["heroku_api_key"])
+      heroku_client = Heroku::Client.new(config[:heroku_user], config[:heroku_api_key])
       heroku_client.console(app, "Dalli::Client.new.flush")
     end
   end
--- lib/herohook/version.rb
+++ lib/herohook/version.rb
@@ -1,3 +1,3 @@
 module Herohook
-  Version = VERSION = '0.0.4'
+  Version = VERSION = '0.0.5'
 end
